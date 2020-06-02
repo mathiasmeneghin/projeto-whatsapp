863b987fca14e0c7f42f5ec57fdc3adbddf9992d
mathiasmeneghin <mathias.meneghin@hotmail.com>
2020-06-02 14:47:53 -0300

    Feito funcionalidade para clicar na aba conversa e ir para o chat do contato selecionado.

--- app/src/main/java/com/mathias/whatsapp/fragment/ConversasFragment.java
+++ app/src/main/java/com/mathias/whatsapp/fragment/ConversasFragment.java
@@ -1,5 +1,6 @@
 package com.mathias.whatsapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -17,8 +19,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 import com.mathias.whatsapp.R;
+import com.mathias.whatsapp.activity.ChatActivity;
 import com.mathias.whatsapp.adapter.ConversasAdapter;
 import com.mathias.whatsapp.config.configuracaoFirebase;
+import com.mathias.whatsapp.helper.RecyclerItemClickListener;
 import com.mathias.whatsapp.helper.UsuarioFirebase;
 import com.mathias.whatsapp.model.Conversa;
 import com.mathias.whatsapp.model.Usuario;
@@ -59,6 +63,31 @@ public class ConversasFragment extends Fragment {
         recyclerViewConversas.setLayoutManager(layoutManager);
         recyclerViewConversas.setHasFixedSize(true);
         recyclerViewConversas.setAdapter(adapter);
+
+        //Configurar Evento de clique
+        recyclerViewConversas.addOnItemTouchListener(new RecyclerItemClickListener(
+                getActivity(),
+                recyclerViewConversas,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Conversa conversaSelecionada = listaConversas.get(position);
+                        Intent i = new Intent(getActivity(), ChatActivity.class);
+                        i.putExtra("chatContato",conversaSelecionada.getUsuarioExibicao());
+                        startActivity(i);
+                    }
+
+                    @Override
+                    public void onLongItemClick(View view, int position) {
+
+                    }
+
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    }
+                }
+        ));
 
         //Configura conversasref
         String identificadorUsuario = UsuarioFirebase.getIdentificadorUsuario();
